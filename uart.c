adbebcc79bb8a783dc0ffbf5cf1459dbcc4fc22d
a1157857220 <46993523+a1157857220@users.noreply.github.com>
2023-12-24 21:26:09 +0800

    Add files via upload
    這是用到的相關設定，header檔都是與Lab10的header檔一樣，控制馬達的轉動的函數是在uart.c裡面的servoControl，參數有三個，用到的channel id，想轉動的角度(-90~90度)，轉動的時間(1秒設1000，1.5秒設1500)。

---
+++ uart.c
@@ -0,0 +1,132 @@
+#include <xc.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+    //setting TX/RX
+
+char mystring[20];
+int lenStr = 0;
+
+void UART_Initialize() {
+           
+    /*       TODObasic   
+           Serial Setting      
+        1.   Setting Baud rate
+        2.   choose sync/async mode 
+        3.   enable Serial port (configures RX/DT and TX/CK pins as serial port pins)
+        3.5  enable Tx, Rx Interrupt(optional)
+        4.   Enable Tx & RX
+    */
+    
+    // UART ??
+    TRISCbits.TRISC6 = 1;            
+    TRISCbits.TRISC7 = 1;            
+    
+    
+    //  Setting baud rate
+    TXSTAbits.SYNC = 0;     //(TXSTA<4>):EUSART Mode Select bit, 0 -> async mode
+    
+     /* BRG16 -> (BAUDCON<3>): choose 16-bit or 8-bit baud rate register
+      a. 16-bit: SPBRGH and SPBRG
+      b. 8-bit: SPBRG only
+      SPBRGH:SPBRG: control the period of a free-running timer
+     */
+    BAUDCONbits.BRG16 = 0;         
+    TXSTAbits.BRGH = 1;     //(TXSTA<2>):High Baud Rate Select bit
+    SPBRG = 51;      
+    
+    
+   //   Serial enable
+    // TRMT -> (TXSTA<7>): Read only, which is set when the TSR register is empty
+    // TSR register: ???????
+    // RSR register: ?????????
+    // TXREG register: ?????????
+    // RCREG register: ?????????
+    RCSTAbits.SPEN = 1;     //(RCSTA<7>:Enable asynchronous port         
+    PIR1bits.TXIF = 0;      //(PIR1<4>):Set when TXREG is empty -> TXPEG ??????????
+    PIR1bits.RCIF = 1;      //(RIR1<5>):will set when reception is complete
+    TXSTAbits.TXEN = 1;     //(TXSTA<5>):enable transmission        
+    RCSTAbits.CREN = 1;     //continuous receive enable bit,will be cleared when error occurred        
+    PIE1bits.TXIE = 0;       //(PIE4):enable interrupt
+    IPR1bits.TXIP = 1;             
+    PIE1bits.RCIE = 0;              
+    //IPR1bits.RCIP = 0; 
+    }
+
+void UART_Write(unsigned char data)  // Output on Terminal
+{
+    while(!TXSTAbits.TRMT);
+    TXREG = data;              //write to TXREG will send data 
+}
+
+
+void UART_Write_Text(char* text) { // Output on Terminal, limit:10 chars
+    for(int i=0;text[i]!='\0';i++)
+        UART_Write(text[i]);
+}
+
+void ClearBuffer(){
+    for(int i = 0; i < 10 ; i++)
+        mystring[i] = '\0';
+    lenStr = 0;
+}
+
+void MyusartRead()
+{
+    /* TODObasic: try to use UART_Write to finish this function */
+    if(RCREG == '\r'){
+        UART_Write('\n');
+    }else{
+        UART_Write(RCREG);
+    }
+    return ;
+}
+
+char *GetString(){
+    return mystring;
+}
+
+
+// void interrupt low_priority Lo_ISR(void)
+void __interrupt(low_priority)  Lo_ISR(void)
+{
+    
+    /*
+    if(RCIF)
+    {
+        if(RCSTAbits.OERR)
+        {
+            CREN = 0;
+            Nop();
+            CREN = 1;
+        }
+        
+        MyusartRead();
+    }
+    */
+    
+   // process other interrupt sources here, if required
+    return;
+}
+
+void servoControl(int id,int degree,int speed){
+    char command[50] = "#";
+    char id_string[10];
+    sprintf(id_string,"%d",id);
+    strcat(command,id_string);
+    strcat(command,"P");
+    
+    char degree_string[10];
+    degree = 11*degree + 1500;
+    sprintf(degree_string,"%d",degree);
+    strcat(command,degree_string);
+    strcat(command,"T");
+    
+    char speed_string[10];
+    sprintf(speed_string,"%d",speed);
+    strcat(command,speed_string);
+    strcat(command,"\r\n");
+    
+    UART_Write_Text(command);     
+    return;
+}
